39d89ae24a306caff7374c981ade95a14260a40c
MalikWahid <wahidmalik551@gmail.com>
2021-08-04 21:31:58 +0500

    Attach firebase & Sign in with google.

--- lib/auth/signIn.dart
+++ lib/auth/signIn.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:food_app/screens/HomeScreen/Home_Screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  Future<void> _googleSignUp() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
+        scopes: <String>['email'],
+      );
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser!.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final User? user = (await _auth.signInWithCredential(credential)).user;
+      print('Signed in : ');
+      print(user!.displayName);
+      // userProvider.addUserData(
+      //   currentUser: user,
+      //   userEmail: user.email,
+      //   userImage: user.photoURL,
+      //   userName: user.displayName,
+      // );
+
+      return null;
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,7 +94,13 @@ class SignIn extends StatelessWidget {
                       SignInButton(
                         Buttons.Google,
                         text: "Sign in with Google",
-                        onPressed: () {},
+                        onPressed: () {
+                          _googleSignUp().then((value) {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => HomeScreen()));
+                          });
+                        },
                       ),
                     ],
                   ),
---
+++ lib/screens/HomeScreen/Home_Screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffd6b738),
+      ),
+    );
+  }
+}
