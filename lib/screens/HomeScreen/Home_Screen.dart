700cf81b06dc120ed1e0451a3954cea794ffd68f
MalikWahid <wahidmalik551@gmail.com>
2021-08-07 10:52:15 +0500

    Complete design Home Page.

--- lib/screens/HomeScreen/Home_Screen.dart
+++ lib/screens/HomeScreen/Home_Screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,9 +7,125 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget singleProduct() {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      height: MediaQuery.of(context).size.height * 0.35,
+      width: MediaQuery.of(context).size.width * 0.45,
+      decoration: BoxDecoration(
+        color: Color(0xffd9dad9),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Image.network(
+                'https://pngimg.com/uploads/spinach/spinach_PNG10.png'),
+          ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10.0,
+              vertical: 5.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Fresh Basel',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.black),
+                ),
+                Text(
+                  '50\$/50 Gram',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5.0),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          child: Container(
+                        height: MediaQuery.of(context).size.height * 0.10 / 2,
+                        width: MediaQuery.of(context).size.width * 0.20,
+                        decoration: BoxDecoration(
+                          //color: Colors.black,
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 2.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '50 Gram',
+                                style: TextStyle(
+                                    fontSize: 12.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Icon(
+                                Icons.arrow_downward,
+                                size: 15.0,
+                                color: Colors.orangeAccent,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.10 / 2,
+                          width: MediaQuery.of(context).size.width * 0.20,
+                          decoration: BoxDecoration(
+                            //color: Colors.red,
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.remove,
+                                size: 20.0,
+                                color: Colors.orangeAccent,
+                              ),
+                              Text(
+                                '1',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Icon(
+                                Icons.add,
+                                size: 20.0,
+                                color: Colors.orangeAccent,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffcbcbcb),
       drawer: Drawer(),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
@@ -43,84 +160,143 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-        child: Column(
+        child: ListView(
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.20,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      'https://ak.picdn.net/shutterstock/videos/5039891/thumb/1.jpg'),
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-                color: Colors.red,
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                      flex: 2,
-                      child: Container(
-                        //color: Colors.red,
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(right: 150, bottom: 1),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    color: Color(0xffd1ad17),
-                                    borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(50.0),
-                                      bottomRight: Radius.circular(50.0),
-                                    )),
-                                height: 50.0,
-                                width: 100.0,
-                                child: Center(
-                                  child: Text(
-                                    'Vegi',
-                                    style: TextStyle(
-                                        fontSize: 22.0,
-                                        color: Colors.white,
-                                        shadows: [
-                                          BoxShadow(
-                                            color: Colors.green.shade800,
-                                            blurRadius: 10,
-                                            offset: Offset(3, 3),
-                                          ),
-                                        ]),
-                                    //textAlign: TextAlign.center,
+            Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://ak.picdn.net/shutterstock/videos/5039891/thumb/1.jpg'),
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.red,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 2,
+                          child: Container(
+                            //color: Colors.red,
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding:
+                                      EdgeInsets.only(right: 150, bottom: 1),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        color: Color(0xffd1ad17),
+                                        borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(50.0),
+                                          bottomRight: Radius.circular(50.0),
+                                        )),
+                                    height: 50.0,
+                                    width: 100.0,
+                                    child: Center(
+                                      child: Text(
+                                        'Vegi',
+                                        style: TextStyle(
+                                            fontSize: 22.0,
+                                            color: Colors.white,
+                                            shadows: [
+                                              BoxShadow(
+                                                color: Colors.green.shade800,
+                                                blurRadius: 10,
+                                                offset: Offset(3, 3),
+                                              ),
+                                            ]),
+                                        //textAlign: TextAlign.center,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(right: 32),
-                              child: Text(
-                                '30% Off',
-                                style: TextStyle(
-                                  fontSize: 40.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
+                                Padding(
+                                  padding: EdgeInsets.only(right: 32),
+                                  child: Text(
+                                    '30% Off',
+                                    style: TextStyle(
+                                      fontSize: 40.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
                                 ),
-                              ),
+                                Text(
+                                  'on all vegetables products',
+                                  style: TextStyle(
+                                    // fontSize: 18.0,
+                                    color: Colors.white,
+                                    //fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                              'on all vegetables products',
-                              style: TextStyle(
-                                // fontSize: 18.0,
-                                color: Colors.white,
-                                //fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )),
-                  Expanded(
-                    child: Container(),
-                  )
-                ],
-              ),
-            )
+                          )),
+                      Expanded(
+                        child: Container(),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Herbs Seasoning',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'View all',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      singleProduct(),
+                      singleProduct(),
+                      singleProduct(),
+                      singleProduct()
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Fresh Fruits',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'View all',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      singleProduct(),
+                      singleProduct(),
+                      singleProduct(),
+                      singleProduct()
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
